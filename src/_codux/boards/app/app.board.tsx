0395b9396d644386cf47d081519a78b966c97da1
Tom Raviv <tomr@wix.com>
2024-01-15 18:05:27 +0200

    dev: flatten layout depth

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -6,6 +6,11 @@ export default createBoard({
     Board: () => <App />,
     isSnippet: true,
     environmentProps: {
-        windowWidth: 1072,
+        windowWidth: 1096,
+        canvasMargin: {
+            left: 0,
+            right: 0,
+            top: 1
+        }
     },
 });
--- src/app.tsx
+++ src/app.tsx
@@ -1,45 +1,7 @@
-import { Balance } from './components/balance/balance';
 import { DashboardLayout } from './components/dashboard-layout/dashboard-layout';
-import { Goals } from './components/goals/goals';
-import { OutcomeStatistics } from './components/outcome-statistics/outcome-statistics';
-import { Section } from './components/section/section';
-import { TransactionHistory } from './components/transaction-history/transaction-history';
-import PageHeader from './components/page-header/page-header';
-import { NewTransaction } from './components/new-transaction/new-transaction';
-import { GetLoanBanner } from './components/get-loan-banner/get-loan-banner';
-import styles from './app.module.scss';
 
 function App() {
-    return (
-        <div className={styles.root}>
-            <DashboardLayout>
-                <Section>
-                    <Section.Content>
-                        <PageHeader className={styles.header}>
-                            <PageHeader.Title>Weekly Summary</PageHeader.Title>
-                        </PageHeader>
-                    </Section.Content>
-                </Section>
-
-                <Section>
-                    <Section.Content className={styles.dashboardGrid}>
-                        <div className={styles.mainColumn}>
-                            <Balance />
-                            <TransactionHistory />
-                        </div>
-                        <div className={styles.secondaryColumn}>
-                            <Goals />
-                            <OutcomeStatistics />
-                            <div className={styles.userActionsSection}>
-                                <NewTransaction />
-                                <GetLoanBanner />
-                            </div>
-                        </div>
-                    </Section.Content>
-                </Section>
-            </DashboardLayout>
-        </div>
-    );
+    return <DashboardLayout />;
 }
 
 export default App;
